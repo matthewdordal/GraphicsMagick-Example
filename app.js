f1d209e8a08cc1beb7f695cd4290f0cdf344ddb2
Matthew.Dordal <Matthew.Dordal@target.com>
2016-04-26 13:56:55 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const args = require('yargs').argv;
+const gm = require('gm');
+
+const text = args.text;
+
+var test = gm('./images/confession-bear.png')
+  .resize(500, 500)
+  .fontSize(20)
+  .fill('white')
+  .drawText(20, 20, text, 'Center')
+  .toBuffer('PNG', (err, buffer) => {
+    if (err) return console.log('### err: ',err);
+
+    console.log(`<img src="data:image/png;base64,${buffer.toString('base64')}" />`)
+  });
+
